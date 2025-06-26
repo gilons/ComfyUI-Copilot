b5db2c843e55d8602679cc2a2c9e4590ffd879f2
li <>
2025-06-26 17:43:17 +0800

    feat:bug fix

--- ui/src/components/chat/messages/WorkflowOption.tsx
+++ ui/src/components/chat/messages/WorkflowOption.tsx
@@ -261,7 +261,7 @@ export function WorkflowOption({ content, name = 'Assistant', avatar, latestInpu
                                                             <path strokeLinecap="round" strokeLinejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z" />
                                                         </svg>
                                                     </div>
-                                                    <div className="absolute bottom-full left-0 mb-2 hidden group-hover:block">
+                                                    <div className="absolute bottom-full left-0 mb-2 hidden group-hover:!block">
                                                         <div className="bg-gray-900 text-white text-xs rounded-md py-2 px-3 min-w-[400px] whitespace-normal break-words">
                                                             {workflow.description}
                                                         </div>
